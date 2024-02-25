871d11c26613e73b51e18e65c50439abfa933c92
Rafael Caixeta <8386288+rmcaixeta@users.noreply.github.com>
2024-02-25 17:15:31 +1100

    Fix after GeoStatsFunction update

--- src/estimators/kriging.jl
+++ src/estimators/kriging.jl
@@ -189,7 +189,7 @@ function local_fit(model_::LocalKrigingModel, data; i, m)
   FLHS = GeoStatsModels.factorize(model, LHS)
 
   # variance type
-  VARTYPE = GeoStatsFunctions.result_type(γ, first(D), first(D))
+  VARTYPE = GeoStatsFunctions.returntype(γ, first(D), first(D))
 
   # record Kriging state
   state = KrigingState(data, FLHS, RHS, VARTYPE)
@@ -235,7 +235,7 @@ function local_lhs(model::KrigingModel, domain, localaniso)
 
   # pre-allocate memory for LHS
   u = first(domain)
-  T = GeoStatsFunctions.result_type(γ, u, u)
+  T = GeoStatsFunctions.returntype(γ, u, u)
   m = nobs + ncons
   LHS = Matrix{T}(undef, m, m)
 
